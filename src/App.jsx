af5ce8b912220fdcfae7ac869ae3d574cf2c7a4b
Ashish Kumar Nayak <ashish0987aashish@gmail.com>
2024-09-03 22:34:05 +0530

    responsiveness improved

--- src/App.jsx
+++ src/App.jsx
@@ -23,7 +23,7 @@ const App = () => {
   }, [query, units]);
 
   return (
-    <div className="mx-auto h-auto w-full sm:h-screen lg:h-screen xl:h-auto  xl:w-3/5 lg:my-5 lg:py-5 lg:px-32 bg-gradient-to-br shadow-xl shadow-gray-400 from-cyan-600  to-blue-800">
+    <div className="mx-auto h-auto w-full sm:h-screen lg:h-screen xl:h-auto  xl:w-3/5 xl:my-5 lg:py-5 lg:px-32 bg-gradient-to-br shadow-xl shadow-gray-400 from-cyan-600  to-blue-800">
       <TopButtons setQuery={setQuery} />
       <Inputs setQuery={setQuery} query={query} setUnits={setUnits} />
 
